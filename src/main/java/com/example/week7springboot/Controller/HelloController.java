c6e475c8f3cf3c8d18d3745cfb1b7effc666bdb3
TajPhutela <tajphutela@gmail.com>
2025-04-19 14:45:05 +0530

    UC3 Added

--- src/main/java/com/example/week7springboot/Controller/HelloController.java
+++ src/main/java/com/example/week7springboot/Controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.example.week7springboot.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-// Marks this as a REST Controller
 @RestController
 public class HelloController {
 
-    // GET endpoint to receive a name as query parameter
-    @GetMapping("/hello/query")
-    public String sayHelloWithQuery(@RequestParam String name) {
+    // Handle GET request with name as path variable
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
