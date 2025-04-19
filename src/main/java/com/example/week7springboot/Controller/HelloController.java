9ff46526947ff1737649f5dd542b2834a2e348c4
TajPhutela <tajphutela@gmail.com>
2025-04-19 14:58:10 +0530

    Uc5 Added

--- src/main/java/com/example/week7springboot/Controller/HelloController.java
+++ src/main/java/com/example/week7springboot/Controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.week7springboot.Controller;
 
-import com.example.week7springboot.dto.UserDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    // POST method to receive JSON body and respond with greeting
-    @PostMapping("/hello/post")
-    public String sayHelloPost(@RequestBody UserDTO user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    // PUT method using path variable and query parameter
+    @PutMapping("/hello/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
